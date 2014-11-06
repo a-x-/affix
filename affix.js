47801aa4b756d482a0626f127de0e164164dafc7
Александр <a-x-@users.noreply.github.com>
2014-11-07 00:47:23 +0300

    Init draft affix lib
    I want create bootstrap like API for affix in the future

---
+++ affix.js
@@ -0,0 +1,14 @@
+function affix(element, top) {
+  window.addEventListener('scroll', function() {
+  	var scrollTop = window.scrollY;
+  	if( scrollTop > top ){
+  		element.classList.add('affix-top');
+  		element.classList.remove('affix');
+  	}
+  	else
+  	{
+  		element.classList.remove('affix-top');
+  		element.classList.add('affix');
+  	}
+  });
+}
